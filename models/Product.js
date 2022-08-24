712f63057378cdcb7a2eff8229222d154f1078ad
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 22:19:23 -0500

    Updating product.js

--- models/Product.js
+++ models/Product.js
@@ -9,10 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-    id: {},
-    product_name: {},
-    price: {},
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    price: {
+      type: DataTypes.DECIMAL(5,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        id: 'id'
+      }
+    }
   },
   {
     sequelize,
